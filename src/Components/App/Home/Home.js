e7ad73121b1858e85f8b9220ec74521d4052e804
abelmuro <abelmuro93@gmail.com>
2023-02-14 13:45:16 -0800

    commit

--- src/Components/App/Home/Home.js
+++ src/Components/App/Home/Home.js
@@ -4,9 +4,33 @@ import styles from './styles.module.css';
 function Home() {
 
     return(
-        <>
+        <main className={styles.container}>
 
-        </>
+
+            <section className={styles.headerBackground}>
+                <div className={styles.flex}>
+                    <div className={styles.header}>
+                        <h2 className={styles.titleOne}>
+                            NEW PRODUCT
+                        </h2>
+                        <h1 className={styles.titleTwo}>
+                            XX99 MARK II HEADPHONES
+                        </h1>
+                        <p className={styles.desc}>
+                            Experience natural, lifelike audio and exceptional 
+                            build quality made for the passionate music enthusiast.
+                        </p>
+                        <button className={styles.seeProductButton}>
+                            SEE PRODUCT
+                        </button>
+                    </div>                      
+                </div>
+
+
+
+            </section>
+
+        </main>
     )
 }
 
